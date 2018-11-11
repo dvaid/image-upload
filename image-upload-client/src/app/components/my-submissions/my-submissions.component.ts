2906e881c72bad53678a9ae94f1c6d80a644e130
quantdikshit <quantdikshit@users.noreply.github.com>
2018-11-11 10:26:47 +0530

    fixed my-submissions

--- image-upload-client/src/app/components/my-submissions/my-submissions.component.ts
+++ image-upload-client/src/app/components/my-submissions/my-submissions.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../model/model.user';
+import { Submission } from '../../model/submission';
+import { SubmissionService } from '../../services/submission.service';
 
 @Component({
   selector: 'app-my-submissions',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MySubmissionsComponent implements OnInit {
 
-  constructor() { }
+  currentUser: User;
+  submissions: Submission[] = [];
 
-  ngOnInit() {
+  constructor(public submissionService: SubmissionService) {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
   }
 
+  ngOnInit() {
+    this.submissionService.getSubmissions(this.currentUser)
+      .subscribe(
+        data => {
+          this.submissions = data;
+        },
+        error => {
+        });
+  }
 }
